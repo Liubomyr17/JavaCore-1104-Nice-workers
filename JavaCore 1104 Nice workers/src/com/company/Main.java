9fb566fb54526a20ad746abbd32221bdfacc2e3d
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-27 11:20:45 +0200

    Add files via upload

---
+++ JavaCore 1104 Nice workers/src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+/*
+
+1104 Nice workers
+Write four classes: Employee (employee), Manager (manager), Chief (director) and Secretary (secretary).
+Inherit a manager, director, and secretary from an employee.
+Requirements:
+1. The Solution class must have a public Employee class.
+2. The Solution class must have a public Manager class.
+3. The Solution class must have a public Chief class (director).
+4. The Solution class must have a public Secretary class.
+5. The Manager class must be inherited from the Employee class.
+6. The Chief class must be inherited from the Employee class.
+7. The Secretary class must be inherited from the Employee class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+    }
+
+    public class Chief extends Employee {
+    }
+
+    public class Employee {
+    }
+
+    public class Secretary extends Employee {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
